219213af0554e14cf5b8fe539244bcbfda0188d1
Vinay Kumar <vinaykhebbar@gmail.com>
2016-06-09 22:22:46 +0530

    remove console.log hijacked

--- test/common.js
+++ test/common.js
@@ -8,22 +8,3 @@ var consoleTransportOpts = {
 
 logger.remove(logger.transports.Console)
 logger.add(logger.transports.Console, consoleTransportOpts)
-
-var consoleOpts = ['log', 'profile', 'startTimer']
-consoleOpts.concat(Object.keys(logger.levels))
-  .forEach(function (method) {
-    console[method] = function () {
-      return logger[method].apply(logger, arguments)
-    }
-  })
-  
-var log = console.log
-console.log = function hijacked_log (level) {
-  if (arguments.length > 1 && level in this) {
-    log.apply(this, arguments)
-  } else {
-    var args = Array.prototype.slice.call(arguments)
-    args.unshift('info')
-    log.apply(this, args)
-  }
-}
