65423f4a0f877d224d2b28392675aa0c6c1ac540
vishal023 <v.singh.kld@gmail.com>
2023-01-10 13:14:57 +0530

    revert?

--- test/test.routes.js
+++ test/test.routes.js
@@ -3,70 +3,103 @@
 var testUtil = require('./util')
 
 var port = 7000
-var customPort = 7001
-const baseURL = 'http://localhost:' + port
-var functionURL = baseURL + '/function'
+var functionURL = 'http://localhost:' + port + '/function'
 var systemToken = 'INTEGRATOR_EXTENSION_SYSTEM_TOKEN'
 var bearerToken = 'ott873f2beed978433997c42b4e5af05d9b'
-const openConnectionsURL = baseURL + '/openConnections'
-const stopServerURL = 'http://localhost:' + customPort + '/stopServer'
 
-describe('routes tests', function () {
-  describe('Express /function route tests', function () {
-    before(function (done) {
-      testUtil.createMockExpressServer(true, false, done)
-    })
+describe('Express /function route tests', function () {
+  before(function (done) {
+    testUtil.createMockExpressServer(true, false, done)
+  })
 
-    it('should return error when request doesn\'t have function field set.', function (done) {
-      var options = {
-        diy: true,
-        type: 'hook',
-        options: {
-          key1: [ 'abc' ],
-          key2: { k: 'v' },
-          _importId: '56eae39e9a016a71a8a9c7e4',
-          bearerToken: bearerToken
-        }
+  it('should return error when request doesn\'t have function field set.', function (done) {
+    var options = {
+      diy: true,
+      type: 'hook',
+      options: {
+        key1: [ 'abc' ],
+        key2: { k: 'v' },
+        _importId: '56eae39e9a016a71a8a9c7e4',
+        bearerToken: bearerToken
+      }
+    }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+      res.statusCode.should.equal(422)
+
+      var expected = {
+        code: 'missing_required_field',
+        message: 'Missing required field function in the extension options.'
       }
 
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
-        res.statusCode.should.equal(422)
+      body.errors[0].should.eql(expected)
+      done()
+    })
+  })
 
-        var expected = {
-          code: 'missing_required_field',
-          message: 'Missing required field function in the extension options.'
+  it('should return error when request doesn\'t have options field set.', function (done) {
+    var options =
+      { diy: true,
+        type: 'hook',
+        function: 'doSomething'
+      }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+      res.statusCode.should.equal(422)
+
+      var expected = {
+        code: 'missing_required_field',
+        message: 'Function options haven\'t been provided.'
+      }
+
+      body.errors[0].should.eql(expected)
+      done()
+    })
+  })
+
+  it('should return error when request doesn\'t have either of diy or _connectorId fields set.', function (done) {
+    var options = {
+      type: 'hook',
+      function: 'doSomething',
+      options: {
+        key1: [ 'abc' ],
+        key2: { k: 'v' },
+        _importId: '56eae39e9a016a71a8a9c7e4',
+        bearerToken: bearerToken
+      }
+    }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+      res.statusCode.should.equal(422)
+
+      var expected = {
+        code: 'missing_required_field',
+        message: 'Need to set either the diy or _connectorId field in extension options.'
+      }
+
+      body.errors[0].should.eql(expected)
+      done()
+    })
+  })
+
+  describe('DIY and connectors test', function () {
+    before(function (done) {
+      testUtil.stopMockExpressServer(function (err) {
+        if (err) {
+          err.message.should.equal('Integration-extension-server not deployed.')
+          err.code.should.equal('invaid_function_call')
         }
-
-        body.errors[0].should.eql(expected)
         done()
       })
     })
 
-    it('should return error when request doesn\'t have options field set.', function (done) {
-      var options =
-        { diy: true,
-          type: 'hook',
-          function: 'doSomething'
-        }
-
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
-        res.statusCode.should.equal(422)
-
-        var expected = {
-          code: 'missing_required_field',
-          message: 'Function options haven\'t been provided.'
-        }
-
-        body.errors[0].should.eql(expected)
-        done()
-      })
-    })
-
-    it('should return error when request doesn\'t have either of diy or _connectorId fields set.', function (done) {
+    it('should return error when diy is set in request but the diy is not set in the integrator extension server.', function (done) {
       var options = {
         type: 'hook',
+        diy: true,
         function: 'doSomething',
         options: {
           key1: [ 'abc' ],
@@ -76,238 +109,160 @@ describe('routes tests', function () {
         }
       }
 
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
-        res.statusCode.should.equal(422)
+      testUtil.createMockExpressServer(false, true, function (err) {
+        if (err) return done(err)
 
-        var expected = {
-          code: 'missing_required_field',
-          message: 'Need to set either the diy or _connectorId field in extension options.'
-        }
+        var functionURL1 = 'http://localhost:' + port + '/function'
+        testUtil.postRequest(functionURL1, options, systemToken, function (error, res, body) {
+          if (error) return done(error)
+          res.statusCode.should.equal(422)
 
-        body.errors[0].should.eql(expected)
-        done()
+          var expected = {
+            code: 'missing_required_field',
+            message: 'DIY is not configured in the extension configuration.'
+          }
+
+          body.errors[0].should.eql(expected)
+          testUtil.stopMockExpressServer(done)
+        })
       })
     })
 
-    describe('DIY and connectors test', function () {
-      before(function (done) {
-        testUtil.stopMockExpressServer(function (err) {
-          if (err) {
-            err.message.should.equal('Integration-extension-server not deployed.')
-            err.code.should.equal('invaid_function_call')
-          }
-          done()
-        })
-      })
-
-      it('should return error when diy is set in request but the diy is not set in the integrator extension server.', function (done) {
-        var options = {
-          type: 'hook',
-          diy: true,
-          function: 'doSomething',
-          options: {
-            key1: [ 'abc' ],
-            key2: { k: 'v' },
-            _importId: '56eae39e9a016a71a8a9c7e4',
-            bearerToken: bearerToken
-          }
-        }
-
-        testUtil.createMockExpressServer(false, true, function (err) {
-          if (err) return done(err)
-
-          var functionURL1 = 'http://localhost:' + port + '/function'
-          testUtil.postRequest(functionURL1, options, systemToken, function (error, res, body) {
-            if (error) return done(error)
-            res.statusCode.should.equal(422)
-
-            var expected = {
-              code: 'missing_required_field',
-              message: 'DIY is not configured in the extension configuration.'
-            }
-
-            body.errors[0].should.eql(expected)
-            testUtil.stopMockExpressServer(done)
-          })
-        })
-      })
-
-      it('should return error when connectors doesn\'t exist but _connectorId is set.', function (done) {
-        var options = {
-          type: 'installer',
-          diy: false,
-          connectorId: '12ege39f7a016v72a8b9c79q',
-          function: 'doSomething',
-          options: {
-            key1: [ 'abc' ],
-            key2: { k: 'v' },
-            _importId: '56eae39e9a016a71a8a9c7e4',
-            bearerToken: bearerToken
-          }
-        }
-
-        testUtil.createMockExpressServer(false, true, function (err) {
-          if (err) return done(err)
-
-          var functionURL1 = 'http://localhost:' + port + '/function'
-          testUtil.postRequest(functionURL1, options, systemToken, function (error, res, body) {
-            if (error) return done(error)
-            res.statusCode.should.equal(422)
-
-            var expected = {
-              code: 'missing_required_field',
-              message: 'Need to set either the diy or _connectorId field in extension options.'
-            }
-
-            body.errors[0].should.eql(expected)
-            testUtil.stopMockExpressServer(done)
-          })
-        })
-      })
-
-      after(function (done) {
-        testUtil.createMockExpressServer(true, false, done)
-      })
-    })
-
-    it('should fail with 401 for wrong system token.', function (done) {
+    it('should return error when connectors doesn\'t exist but _connectorId is set.', function (done) {
       var options = {
-        diy: true,
         type: 'installer',
-        function: 'runInstallerSuccessStep',
+        diy: false,
+        connectorId: '12ege39f7a016v72a8b9c79q',
+        function: 'doSomething',
         options: {
-          key: 'value',
-          bearerToken: bearerToken,
-          _integrationId: '56eae39e9a016a71a8a9c7e4'
-        }
-      }
-
-      testUtil.postRequest(functionURL, options, 'BAD_INTEGRATOR_EXTENSION_SYSTEM_TOKEN', function (error, res, body) {
-        if (error) return done(error)
-
-        res.statusCode.should.equal(401)
-        res.headers['WWW-Authenticate'.toLowerCase()].should.equal('invalid system token')
-        var expected = { errors: [{ code: 'unauthorized', message: 'Invalid system token.' }] }
-        body.should.eql(expected)
-
-        done()
-      })
-    })
-
-    it('should fail with 422 when response exceeds max size.', function (done) {
-      var options = {
-        diy: true,
-        type: 'hook',
-        function: 'echoResponse',
-        maxResponseSize: 2,
-        options: {
-          resp: ['abc', 'abc'],
+          key1: [ 'abc' ],
+          key2: { k: 'v' },
+          _importId: '56eae39e9a016a71a8a9c7e4',
           bearerToken: bearerToken
         }
       }
 
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
+      testUtil.createMockExpressServer(false, true, function (err) {
+        if (err) return done(err)
 
-        res.statusCode.should.equal(422)
-        var expected = { errors: [{'code': 'response_size_exceeded', 'message': 'response stream exceeded limit of 2 bytes.'}] }
+        var functionURL1 = 'http://localhost:' + port + '/function'
+        testUtil.postRequest(functionURL1, options, systemToken, function (error, res, body) {
+          if (error) return done(error)
+          res.statusCode.should.equal(422)
 
-        body.should.eql(expected)
-        done()
-      })
-    })
+          var expected = {
+            code: 'missing_required_field',
+            message: 'Need to set either the diy or _connectorId field in extension options.'
+          }
 
-    it('should fail with 422 when response is not serializable.', function (done) {
-      var options = {
-        diy: true,
-        type: 'hook',
-        function: 'respondWithNonSearializableObject',
-        maxResponseSize: 2000,
-        options: {
-          key1: ['abc'],
-          bearerToken: bearerToken
-        }
-      }
-
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
-
-        res.statusCode.should.equal(422)
-        var expected = { errors: [{'code': 'invalid_extension_response', 'message': 'Extension response is not serializable.'}] }
-
-        body.should.eql(expected)
-        done()
-      })
-    })
-
-    it('should pass when request size is greater than default 100kb.', function (done) {
-      var largeString = 'a'
-      for (var i = 0; i < 4000000; i++) {
-        largeString += 'a'
-      }
-
-      var options = {
-        diy: true,
-        type: 'hook',
-        function: 'echoResponse',
-        maxResponseSize: 2000,
-        options: {
-          key1: [largeString],
-          bearerToken: bearerToken
-        }
-      }
-
-      testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
-        if (error) return done(error)
-
-        res.statusCode.should.equal(200)
-
-        done()
+          body.errors[0].should.eql(expected)
+          testUtil.stopMockExpressServer(done)
+        })
       })
     })
 
     after(function (done) {
-      testUtil.stopMockExpressServer(done)
-    })
-  })
-
-  describe('Express /openConnections route tests', function () {
-
-    before((done)=>{
       testUtil.createMockExpressServer(true, false, done)
     })
-
-    after((done)=>{
-      testUtil.stopMockExpressServer(done)
-    })
-
-    it('should return the no of active connections', (done) => {
-      testUtil.getRequest(openConnectionsURL, systemToken, (err, response, body) => {
-        if (err) return done(err)
-        response.statusCode.should.equal(200)
-        body.should.equal(0)
-        done()
-      })
-    })
   })
 
-  describe('Express /stopServer route tests', function () {
-    const configs = {
-      timeout: 0,
-      port: 7001
+  it('should fail with 401 for wrong system token.', function (done) {
+    var options = {
+      diy: true,
+      type: 'installer',
+      function: 'runInstallerSuccessStep',
+      options: {
+        key: 'value',
+        bearerToken: bearerToken,
+        _integrationId: '56eae39e9a016a71a8a9c7e4'
+      }
     }
 
-    before((done)=>{
-      testUtil.createCustomMockExpressServer(configs,true, false, done)
-    })
+    testUtil.postRequest(functionURL, options, 'BAD_INTEGRATOR_EXTENSION_SYSTEM_TOKEN', function (error, res, body) {
+      if (error) return done(error)
 
-    it('should begin killing the server', (done) => {
-      testUtil.getRequest(stopServerURL, systemToken, (err, response) => {
-        if (err) return done(err)
-        response.statusCode.should.equal(200)
-        done()
-      })
+      res.statusCode.should.equal(401)
+      res.headers['WWW-Authenticate'.toLowerCase()].should.equal('invalid system token')
+      var expected = { errors: [{ code: 'unauthorized', message: 'Invalid system token.' }] }
+      body.should.eql(expected)
+
+      done()
     })
   })
-});
+
+  it('should fail with 422 when response exceeds max size.', function (done) {
+    var options = {
+      diy: true,
+      type: 'hook',
+      function: 'echoResponse',
+      maxResponseSize: 2,
+      options: {
+        resp: ['abc', 'abc'],
+        bearerToken: bearerToken
+      }
+    }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+
+      res.statusCode.should.equal(422)
+      var expected = { errors: [{'code': 'response_size_exceeded', 'message': 'response stream exceeded limit of 2 bytes.'}] }
+
+      body.should.eql(expected)
+      done()
+    })
+  })
+
+  it('should fail with 422 when response is not serializable.', function (done) {
+    var options = {
+      diy: true,
+      type: 'hook',
+      function: 'respondWithNonSearializableObject',
+      maxResponseSize: 2000,
+      options: {
+        key1: ['abc'],
+        bearerToken: bearerToken
+      }
+    }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+
+      res.statusCode.should.equal(422)
+      var expected = { errors: [{'code': 'invalid_extension_response', 'message': 'Extension response is not serializable.'}] }
+
+      body.should.eql(expected)
+      done()
+    })
+  })
+
+  it('should pass when request size is greater than default 100kb.', function (done) {
+    var largeString = 'a'
+    for (var i = 0; i < 4000000; i++) {
+      largeString += 'a'
+    }
+
+    var options = {
+      diy: true,
+      type: 'hook',
+      function: 'echoResponse',
+      maxResponseSize: 2000,
+      options: {
+        key1: [largeString],
+        bearerToken: bearerToken
+      }
+    }
+
+    testUtil.postRequest(functionURL, options, systemToken, function (error, res, body) {
+      if (error) return done(error)
+
+      res.statusCode.should.equal(200)
+
+      done()
+    })
+  })
+
+  after(function (done) {
+    testUtil.stopMockExpressServer(done)
+  })
+})
